c1ac7039a04068dd1bc1e7f57ab01313e5963edb
Mustafa Khamisi <mustafangure95@gmail.com>
2022-07-24 23:51:35 -0500

    feat: Remember the coin that was previously used

--- recursion/numChange2.py
+++ recursion/numChange2.py
@@ -1,14 +1,25 @@
-def makeChange(coins_list, change, min_coins):
+def makeChange(coins_list, change, min_coins, coins_used):
     for cents in range(change + 1):
         coin_count = cents
+        newCoin = 1
         for j in [c for c in coins_list if c <= cents]:
-            # print(f'cc={coin_count} cents={cents} j={j} min_coins={min_coins}')
             if min_coins[cents-j] + 1 < coin_count:
                 coin_count = min_coins[cents-j] + 1
+                newCoin = j
         min_coins[cents] = coin_count
-        print(min_coins)
+        coins_used[cents] = newCoin
+    printCoins(coins_used, change)
+    # print(coins_used[change])
     return min_coins[change]
 
 
+def printCoins(coinsUsed, change):
+    coins = change
+    while coins > 0:
+        thisCoin = coinsUsed[coins]
+        print(thisCoin)
+        coins = coins - thisCoin
+
+
 if __name__ == "__main__":
-    print(makeChange([1, 5, 10, 21, 25], 63, [0]*64))
+    print(makeChange([1, 5, 10, 21, 25], 63, [0]*64, [0]*64))
